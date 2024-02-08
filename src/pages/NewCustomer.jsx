22183ae1cc6d4e9b98e4701c1eed1a8ab2d2bc3d
Gboluwaga Alamu <alamugboluwaga402@gmail.com>
2024-02-08 22:24:27 +0100

    Input

--- src/pages/NewCustomer.jsx
+++ src/pages/NewCustomer.jsx
@@ -1,6 +1,18 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const NewCustomer = () => {
+const [user, setUser] = useState({
+  firstName: "",
+  middleName: "",
+  lastName: "",
+  email: "",
+  mobile: "",
+  dob: "",
+  bvn: "",
+  password: "",
+});
+console.log(user);
+
   return (
     <div className="flex flex-col justify-center items-center h-[120vh] w-screen">
       <form className="flex flex-col  gap-6 items-center border h-[100%] w-[100%] bg-pink-00 rounded-sm shadow-sm">
@@ -8,63 +20,63 @@ const NewCustomer = () => {
         <input
           type="text"
           placeholder="First Name"
-          onChange={""}
+          onChange={(e) => setUser({ ...user, firstName: e.target.value })}
           required
           className="h-[5%] w-[70%] rounded pl-4 outline-none border  focus:border-red-200"
         />
         <input
           type="text"
           placeholder="Middle Name"
-          onChange={""}
+          onChange={(e) => setUser({ ...user, middleName: e.target.value })}
           required
           className="h-[5%] w-[70%] rounded pl-4 outline-none border focus: focus:border-red-200"
         />
         <input
           type="text"
           placeholder="Last Name"
-          onChange={""}
+          onChange={(e) => setUser({ ...user, lastName: e.target.value })}
           required
           className="h-[5%] w-[70%] rounded pl-4 outline-none border  focus:border-red-200"
         />
         <input
           type="email"
           placeholder="Email"
-          onChange={""}
+          onChange={(e) => setUser({ ...user, email: e.target.value })}
           required
           className="h-[5%] w-[70%] rounded pl-4 outline-none border  focus:border-red-200"
         />
         <input
           type="text"
           placeholder="Moble Number"
-          onChange={""}
+          onChange={(e) => setUser({ ...user, mobile: e.target.value })}
           required
           className="h-[5%] w-[70%] rounded pl-4 outline-none border  focus:border-red-200"
         />
         <input
           type="date"
           placeholder="Date of Birth"
-          onChange={""}
+          onChange={(e) => setUser({ ...user, dob: e.target.value })}
           required
           className="h-[5%] w-[70%] rounded pl-4 outline-none border  focus:border-red-200"
         />
         <input
           type="text"
           placeholder="B V N"
-          onChange={""}
+          onChange={(e) => setUser({ ...user, bvn: e.target.value })}
           required
           className="h-[5%] w-[70%] rounded pl-4 outline-none border  focus:border-red-200"
         />
         <input
           type="password"
           placeholder="Password"
-          onChange={""}
+          onChange={(e) => setUser({ ...user, password: e.target.value })}
           required
           className="h-[5%] w-[70%] rounded pl-4 outline-none border  focus:border-red-200"
         />
         <input
           type="password"
           placeholder="Confirm password"
-          onChange={""}
+          onChange={(e) => setUser({ ...user, confirmName: e.target.value })}
           required
           className="h-[5%] w-[70%] rounded pl-4 outline-none border  focus:border-red-200"
         />
