0744338999bf8707562aa2d2ecf2605616e50263
Gboluwaga Alamu <alamugboluwaga402@gmail.com>
2024-02-08 21:07:20 +0100

    initial Commit

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,95 +1,11 @@
-import React, { useEffect, useState } from 'react'
-import { containerStyle } from '../componenets/Components'
-import axios from 'axios'
-import { useDispatch, useSelector } from 'react-redux';
-import { fetchData } from "../slice/DataFetchSlice";
-const api = import.meta.env.VITE_API;
-//   const url = import.meta.env.VITE_URL;
+import React from 'react'
+
 const Login = () => {
-   const users = useSelector((state) => state.dataState.user);
-    const [user,setUser]=useState({
-        name:'',
-        email:'',
-        mobile:'',
-        password:"",
-        confirmPassword:""
-    })
-
-
-const handleSubmit = async()=>{
-    try {
-        if(user.password  !== user.confirmPassword){
-         console.log("password does not macth");
-        }else {
-            const response = await axios.post(`${api}`, user);
-            if (response.status == 200) {
-              console.log("Login Successfull", response.data);
-            } else {
-              console.log("Login Failed !!!", response.data);
-            }
-        }
-        
-    } catch (error) {
-        console.error('Error durinZ  87g login',error.message);
-    }
-
-}
   return (
-    <div className={containerStyle}>
-      Login
-      <form className="flex flex-col gap-6 border rounded p-4 ">
-        <div>
-          <h3 className="text-center">Login</h3>
-        </div>
-        <input
-          className="p-2 rounded outline-none  border f focus:border-red-300"
-          type="text"
-          placeholder="Name"
-          required
-          onChange={(e) => setUser({ ...user, name: e.target.value })}
-        />
-
-        <input
-          className="p-2 rounded outline-none  border  focus:border-red-300"
-          type="email"
-          placeholder="Email"
-          required
-          onChange={(e) => setUser({ ...user, email: e.target.value })}
-        />
-        <input
-          className="p-2 rounded outline-none  border  focus:border-red-300"
-          type="text"
-          placeholder="Mobile"
-          required
-          onChange={(e) => setUser({ ...user, mobile: e.target.value })}
-        />
-        <input
-          className="p-2 rounded outline-none  border f focus:border-red-300"
-          type=""
-          placeholder="Password "
-          required
-          onChange={(e) => setUser({ ...user, password: e.target.value })}
-        />
-        <input
-          className="p-2 rounded outline-none  border focus:border-red-300"
-          type="text"
-          placeholder="Confirm Password"
-          required
-          onChange={(e) => {
-            setUser({ ...user, confirmPassword: e.target.value });
-          }}
-        />
-        <button
-          type="button"
-          onClick={handleSubmit}
-          className="bg-green-500 p-3 rounded text-white active:bg-green-400"
-        >
-          submit
-        </button>
-    
-      </form>
+    <div className=' bg-green-300 h-[120vh] w-screen'>
+      <h1 className='text-center'>Login</h1>
     </div>
-  );
+  )
 }
 
 export default Login
