b47e698a20cef3226dcf08a8b893bfd77c3d093f
Gboluwaga Alamu <alamugboluwaga402@gmail.com>
2024-02-08 15:36:58 +0100

    Youverify initial commit

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,95 +1,15 @@
-import React, { useEffect } from 'react'
-import { useDispatch, useSelector } from 'react-redux'
-import { fetchData } from '../slice/DataFetchSlice'
-import CircularProgress from '@mui/material/CircularProgress';
-import Box from '@mui/material/Box';
-
-// export default function CircularIndeterminate() {
-//   return (
-//     <Box sx={{ display: 'flex' }}>
-//       <CircularProgress />
-//     </Box>
-//   );
-// }
+import React from 'react'
+import NewCustomer from './NewCustomer'
 
 const Home = () => {
-  const dispatch = useDispatch()
-  const users= useSelector(state=>state.dataState.user)
-
-  useEffect(()=>{
-    dispatch(fetchData())
-  },[])
-console.log("USERS",users);
-return (
-  <div className="p-4 flex flex-col justify-center items-center">
-    {users.status === "Pending"  ? (
-"yes"
-    ) : users.length === 0 ? (
-          <h1 className=" text-red-500">No user data was Found</h1>
-    ) : (
-      <div>
-        {users.map((item, index) => (
-          <div key={index} className="flex flex-col gap-1">
-            <label className="flex gap-2 mt-3">
-              First Name :
-              <input
-                type="text"
-                value={item.firstName}
-                disabled
-                className="outline-none border rounded pl-2 pt-1 pb-1 pr-1"
-              />
-            </label>
-            <label className="flex gap-2 mt-3">
-              Middle Name :
-              <input
-                type="text"
-                value={item.middleName}
-                disabled
-                className="outline-none border rounded pl-2 pt-1 pb-1 pr-1"
-              />
-            </label>
-            <label className="flex gap-2 mt-3">
-              Last Name :
-              <input
-                type="text"
-                value={item.lastName}
-                disabled
-                className="outline-none border rounded pl-2 pt-1 pb-1 pr-1"
-              />
-            </label>
-            <label className="flex gap-2 mt-3">
-              Email :
-              <input
-                type="text"
-                value={item.email}
-                disabled
-                className="outline-none border rounded pl-2 pt-1 pb-1 pr-1"
-              />
-            </label>
-            <label className="flex gap-2 mt-3">
-              Date of Birth :
-              <input
-                type="text"
-                value={item.dateOfBirth}
-                disabled
-                className="outline-none border rounded pl-2 pt-1 pb-1 pr-1"
-              />
-            </label>
-            <label className="flex gap-2 mt-3">
-              Mobile :
-              <input
-                type="text"
-                value={item.mobile}
-                disabled
-                className="outline-none border rounded pl-2 pt-1 pb-1 pr-1"
-              />
-            </label>
-          </div>
-        ))}
-      </div>
-    )}
-  </div>
-);
+  return (
+    <div className=" ">
+      <div className="h-[10vh] bg-white  static">Nav</div>
+      Home
+      <div className="h-[100vh]"><NewCustomer/></div>
+      <div className="h-[50vh] bg-green-200">Footer</div>
+    </div>
+  );
 }
 
 export default Home
--- src/slice/NewCustomerSlice.jsx
+++ src/slice/NewCustomerSlice.jsx
@@ -1,38 +1,27 @@
-import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
-import axios from "axios";
+import { createAsyncThunk, createSlice } from "@reduxjs/toolkit"
+import { build } from "vite"
 
-const api = import.meta.env.VITE_API;
 
-export const createCustomer = createAsyncThunk(
-  "newCustomer/creatCustomer",
-  async (user, { rejectWithValue }) => {
-    try {
-      const response = await axios.post(`${api}`, user);
-      console.log(response.data);
-    } catch (error) {
-      console.error(error.message);
-      rejectWithValue(error);
-    }
-  }
-);
+const initialState={
+user:[],
+status:"",
+isLoading:false,
+error:false
+}
 
-const initialState = {
-  firstName: "",
-  middleName: "",
-  lastName: "",
-  email: "",
-  mobile: "",
-  dateOfBirth: "",
-  password: "",
-  confirmPassword: "",
-};
+export const fetchCustomers= createAsyncThunk()
 
-const newCustomerSlice = createSlice({
-  name: "newCustomer",
+
+
+const customerSlice = createSlice({
   initialState,
-  reducers: {
-    postData: (state) => {},
-  },
-});
-export const { postData } = newCustomerSlice.actions;
-export default newCustomerSlice.reducer;
+  reducers:{},
+  extraReducers:()=>{
+    builder.addCase(
+
+    )
+  }
+})
+
+
+export default customerSlice.reducer
